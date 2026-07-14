286a35548f471ec085b2a17911dfadcdbae2377b
barnabaclements <barnabaclements@gmail.com>
2026-07-14 14:56:05 +0530

    Fix login API URL for production

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/login/",
+        "login/",
         {
           username,
           password,
