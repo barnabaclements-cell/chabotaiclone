a71964768dc8cbe7d41775c96280ec5fe4d789a2
barnabaclements <barnabaclements@gmail.com>
2026-07-14 15:33:02 +0530

    Fix JWT login

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from .views import SignupView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+
+    # Login API
+    path(
+        "login/",
+        TokenObtainPairView.as_view(),
+        name="login"
+    ),
+
+    # Refresh token API
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
 ]
